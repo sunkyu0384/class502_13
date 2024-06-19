7348a8a65117c9e12b10c52bc790f81ee702e3c4
sunkyu0384 <sunkyu0384@naver.com>
2024-06-19 12:46:30 +0900

    수업

---
+++ 9. 웹 서버 프로그램 구현/day07/src/test/java/org/choongang/member/tests/JoinServiceTest.java
@@ -0,0 +1,60 @@
+package org.choongang.member.tests;
+
+import com.github.javafaker.Faker;
+import org.choongang.global.exceptions.BadRequestException;
+import org.choongang.member.controllers.RequestJoin;
+import org.choongang.member.services.JoinService;
+import org.choongang.member.validators.JoinValidator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Locale;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("회원가입 가능 테스트")
+public class JoinServiceTest {
+
+    private JoinService service;
+
+    @BeforeEach
+    void init() {
+        service = new JoinService(new JoinValidator());
+    }
+
+    RequestJoin getData() {
+        Faker faker = new Faker(Locale.ENGLISH);
+
+        RequestJoin form = RequestJoin.builder()
+                .email(System.currentTimeMillis() + faker.internet().emailAddress())
+                .password(faker.regexify("\\w{8}").toLowerCase())
+                .userName(faker.name().fullName())
+                .termsAgree(true)
+                .build();
+        form.setConfirmPassword(form.getPassword());
+
+        return form;
+    }
+
+    @Test
+    @DisplayName("회원가입 성공 시 예외가 발생하지 않음")
+    void successTest() {
+        assertDoesNotThrow(() -> {
+            service.process(getData());
+        });
+    }
+
+    @Test
+    @DisplayName("필수 입력 항목(이메일, 비밀번호, 비밀번호 확인, 회원명, 약관 동의) 검증, 검증 실패 시 BadRequestException 발생")
+    void requiredFieldTest() {
+        assertThrows(BadRequestException.class, () -> {
+            RequestJoin form = getData();
+            form.setEmail(null);
+            service.process(form);
+        });
+    }
+}
