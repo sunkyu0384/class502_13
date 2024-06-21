6cb20e10689923e732df5ff75bcfa3633ca7f1c1
sunkyu0384 <sunkyu0384@naver.com>
2024-06-21 13:54:11 +0900

    수업

--- 9. 웹 서버 프로그램 구현/day07/src/main/java/org/choongang/file/controllers/FileSearchController.java
+++ 9. 웹 서버 프로그램 구현/day07/src/main/java/org/choongang/file/controllers/FileSearchController.java
@@ -6,13 +6,37 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @WebServlet("/upload/*")
 public class FileSearchController extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String uri = req.getRequestURI(); // 현재 접속 주소
-        System.out.println(uri);
+        String uri = req.getRequestURI().replace(req.getContextPath(), ""); // 현재 접속 주소
+        // /upload/파일경로
+
+        Pattern pattern = Pattern.compile("^/upload/(.+)");
+        Matcher matcher = pattern.matcher(uri);
+        if (matcher.find()) {
+            String fileName = matcher.group(1);
+
+            File file = new File("C:/uploads/" + fileName);
+            if (file.exists()) {
+
+                try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file))) {
+                    OutputStream out = resp.getOutputStream();
+                    out.write(bis.readAllBytes()
+                    );
+                }
+
+                return;
+            }
+        }
+
+        // 파일이 없는 경우 - 404 응답 코드
+        resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+
     }
 }
