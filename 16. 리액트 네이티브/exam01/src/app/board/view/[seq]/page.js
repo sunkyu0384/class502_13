c7d27250375fe778841f0b1dfc500bc2d50ceaf4
sunkyu0384 <sunkyu0384@naver.com>
2024-09-04 09:50:39 +0900

    수업 소스

---
+++ 16. 리액트 네이티브/exam01/src/app/board/view/[seq]/page.js
@@ -0,0 +1,6 @@
+const BoardView = (props) => {
+    console.log("props", props);
+    return <h1>게시글 보기</h1>;
+}
+
+export default BoardView;
---
+++ 16. 리액트 네이티브/exam01/src/app/member/layout.js
@@ -0,0 +1,10 @@
+export default function MemberLayout({children}) {
+
+    return (
+        <>
+            <h1>회원 전용 레이아웃</h1>
+            {children}
+            <h1>회원 전용 하단 부분</h1>
+        </>
+    );
+}
